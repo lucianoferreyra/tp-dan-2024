afc3836c6c9c2e78233d76e6166e0a3e6bcac275
Luciano J. Ferreyra <luciano.ferreyra@diveria.com>
2025-10-18 15:12:08 -0300

    Add Hibernate5JakartaModule dependency and configure ObjectMapper for lazy loading

--- ms-productos/src/main/java/isi/dan/ms_productos/conf/JacksonConfig.java
+++ ms-productos/src/main/java/isi/dan/ms_productos/conf/JacksonConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+// import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 
 @Configuration
 public class JacksonConfig {
@@ -12,7 +13,9 @@ public class JacksonConfig {
   @Bean
   public ObjectMapper objectMapper() {
     ObjectMapper mapper = new ObjectMapper();
-    mapper.registerModule(new JavaTimeModule());
+    Hibernate5JakartaModule hibernateModule = new Hibernate5JakartaModule();
+    hibernateModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, false);
+    mapper.registerModule(hibernateModule);
     return mapper;
   }
 }
